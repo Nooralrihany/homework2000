4d82d811f14a6db682efb5f50e37065eed919029
Nooralrihany <Nooralrihany@gmail.com>
2024-02-08 01:55:33 +0300

    first commit

--- tasks.js
+++ tasks.js
@@ -2,26 +2,52 @@
 function checkVotingEligibility() {
 
 }
+var myAge=23;
+if (myAge==23) {
+  console.log('you are eligible for voting')
+}
+  else if (myAge < 23) {
+    console.log('you are not availble to voting')
+  }
+
 
 // Task 2
 function printNumbersWithWhile() {
-
+}
+var i=0;
+while (i<6) {
+  console.log(i)
+  i++;
 }
 
 // Task 3
 function printEvenNumbersWithFor() {
 
 }
-
+var i=10;
+while (i / 2) {
+  console.log(i)
+  i++;
+}
 // Task 4
 function getDayOfWeek() {
 
 }
+for (var i=0 ; i < 5 ; i++) {
+  console.log(i , 'sunday')
+  
+  for (var j=5 ; j > 0 ; j--)
+  console.log(j , 'monday')
+}
+
+
 
 // Task 5
-function sum() {
+function sum(number1 , number2) {
 
+  console.log(number1 + number2)
 }
+sumtwo (1 , 2)
 
 // Task 6
 //Uncomment and solve
@@ -35,3 +61,5 @@ module.exports = {
   sum,
   fruits,
 };
+var fruits=['apple' , 'orange' , 'blueberry']
+console.log(fruits)
